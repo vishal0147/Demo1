9673758783aa40201c36ffd4d860b0c62a208b4e
Vishal <Vishal@DESKTOP-0GE7J37>
2022-07-05 20:29:15 +0530

    Check the array

---
+++ src/Demo/Reverse.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4};
+		for(int x=arr.length-1;x>=0;x--) 
+		{
+			System.out.print(arr[x]+" ");
+		}
+		
+	}
+}
+
